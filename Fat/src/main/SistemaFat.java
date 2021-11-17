cdb303c74f2693dfb1231b4022675862ed22728d
Ger1y1 <63232421+Ger1y1@users.noreply.github.com>
2021-11-17 12:21:41 +0100

    To P'ariba

--- Fat/src/main/SistemaFat.java
+++ Fat/src/main/SistemaFat.java
@@ -1,6 +1,5 @@
 package main;
 
-
 public class SistemaFat {
 	Cluster[] Cluster;
 	FAT Fat;
@@ -17,6 +16,27 @@ public class SistemaFat {
 			Entrada.Disponible = true;
 		}
 		DirRaiz.ListaEntradasDirectorios.clear();
+	}
+	
+	public void Mostrar() {
+		System.out.println("Sistema Fat: ");
+		System.out.println("Fat: ");
+		int i = 0, j = 0;
+		for (Entrada_Fat Entrada: Fat.ListaEntradasFat) {
+			System.out.println("Entrada_" + i + ": ");
+			i++;
+			System.out.println("Disponible: " + Entrada.Disponible + " /Siguiente: "  + Entrada.siguiente + " /Fin: " + Entrada.Fin);
+		}
+		System.out.println("Directorio Raiz: ");
+		for (Entrada_Directorio Entrada: DirRaiz.ListaEntradasDirectorios) {
+			System.out.println("Posicion_" + j + ": ");
+			j++;
+			String aux;
+			if (Entrada.esDir) aux = "Directorio";
+			else aux = "Archivo";
+			System.out.println("Nombre: " + Entrada.nombre + " /Tipo: " + aux + " /Cluster Inicio: " + Entrada.ClusterInicio);
+		}
+		
 	}
 }
 
