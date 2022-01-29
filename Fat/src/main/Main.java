a1d36030d5189ca958fbb1e05973af8afe66f9d3
Carlos Daniel <20032000dani@gmail.com>
2022-01-29 23:50:54 +0100

    Interfaz completada
    Se a creado la clase de MenuManager donde se encuentra la interfaz del programa.

--- Fat/src/main/Main.java
+++ Fat/src/main/Main.java
@@ -1,21 +1,22 @@
 package main;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SistemaFat Sistema = new SistemaFat(20);
-
-
-		Sistema.formatear();
-		Sistema.anadirArchivo("peli","C:",3);
-		Sistema.anadirDirectorio("pelis", "C:");
-		Sistema.anadirArchivo("otraPeli", "C:/pelis",2);
-		Sistema.anadirDirectorio("mascosas", "C:");
-		Sistema.anadirArchivo("otraPelimas", "C:/mascosas",2);
-		Sistema.anadirDirectorio("mascosas2", "C:/mascosas");
-		Sistema.anadirDirectorio("mascosas3", "C:/mascosas");
-		Sistema.anadirArchivo("foto","C:/mascosas/mascosas3",1);
+		MenuManager main = new MenuManager();
+		main.mostrarMenu();
+		//Sistema.formatear();
+		//Sistema.anadirArchivo("peli","C:",3);
+		//Sistema.anadirDirectorio("pelis", "C:");
+		//Sistema.anadirArchivo("otraPeli", "C:/pelis",2);
+		//Sistema.anadirDirectorio("mascosas", "C:");
+		//Sistema.anadirArchivo("otraPelimas", "C:/mascosas",2);
+		//Sistema.anadirDirectorio("mascosas2", "C:/mascosas");
+		//Sistema.anadirDirectorio("mascosas3", "C:/mascosas");
+		//Sistema.anadirArchivo("foto","C:/mascosas/mascosas3",1);
 		
 		
 		
@@ -23,11 +24,10 @@ public class Main {
 		//Sistema.eliminarArchivo("C:/mascosas/mascosas2/mascosas3/foto");
 		//Sistema.eliminarArchivo("C:/peli");
 		//Sistema.eliminarDirectorio("C:/mascosas/mascosas2");
-		Sistema.moverArchivo("C:/mascosas/mascosas3/foto/", "C:/pelis/");
+		//Sistema.moverArchivo("C:/mascosas/mascosas3/foto/", "C:/pelis/");
 		
-		Sistema.mostrarDir("C:", 0);
-		Sistema.mostrarFat();
+		//Sistema.mostrarDir("C:", 0);
+		//Sistema.mostrarFat();
 		
 	}
-
 }
---
+++ Fat/src/main/MenuManager.java
@@ -0,0 +1,183 @@
+package main;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuManager {
+	
+	SistemaFat Sistema;
+	ListaProcesos procesos;
+	
+	public MenuManager() {
+		Sistema = new SistemaFat(20);
+		procesos = new ListaProcesos();
+		Sistema.formatear();
+	}
+	
+	public void mostrarMenu() {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+
+        while(!salir){
+            System.out.println("\n=========Escoja una opción el menú=========");
+            System.out.println("1. Crear Archivo");
+            System.out.println("2. Crear Directorio");
+            System.out.println("3. Mostrar Metadatos"); //DANIEL
+            System.out.println("4. Eliminar Archivo"); //GERMAN
+            System.out.println("5. Eliminar Directorio"); //GERMAN
+            System.out.println("6. Mover Archivo");//GER
+            System.out.println("7. Mostrar directorio");
+            System.out.println("8. Matar Proceso");
+            System.out.println("9. Listar procesos");
+            System.out.println("10. Empezar borraTMP");
+            System.out.println("11. Salir\n");
+
+
+            try {
+                opcion = sn.nextInt();
+                switch (opcion) {
+                    case 1:
+                    	clearConsole();
+                    	System.out.println("\nHas seleccionado la opcion 1 \"Crear Archivo\"");
+                    	anadirArchivo();
+                        break;
+                    case 2:
+                    	clearConsole();
+                        System.out.println("\nHas seleccionado la opcion 2 \"Crear Directorio\"");
+                    	crearDir();
+                        break;
+                    case 3:
+                    	clearConsole();
+                        System.out.println("\nHas seleccionado la opcion \"Mostrar Metadatos\"");
+                        mostrarMetaDatos();
+                        break;
+                    case 4:
+                    	clearConsole();
+                        System.out.println("\nHas seleccionado la opcion 4 \"Eliminar Archivo\"");
+                        eliminarArchivo();
+                        break;
+                    case 5:
+                    	clearConsole();
+                        System.out.println("\nHas seleccionado la opcion 5 \"Eliminar Directorio\"");
+                        eliminarDir();
+                        break;
+                    case 6:
+                    	clearConsole();
+                        System.out.println("\nHas seleccionado la opcion 6 \"Mover Archivo\"");
+                        moverArchivo();
+                        break;
+                    case 7:
+                    	clearConsole();
+                    	System.out.println("\nHas seleccionado la opcion 7 \"Mostrar directorio\"");
+                    	mostrarDirectorio();
+                        break;
+                    case 8:
+                    	clearConsole();
+                    	System.out.println("\nHas seleccionado la opcion 8 \"Matar Proceso\"");
+                        matarProceso();
+                        break;
+                    case 9:
+                    	clearConsole();
+                    	System.out.println("\nHas seleccionado la opcion 9 \"Listar procesos\"");
+                    	procesos.imprimirProcesos();
+                        break;
+                    case 10:
+                    	clearConsole();
+                    	System.out.println("\nHas seleccionado la opcion 10 \"Empezar borraTMP\"");
+                    	borrarTmp();
+                        break;
+                    case 11:
+                    	clearConsole();
+                        System.out.println("\nGracias por usar nuestro sistema, hasta luego :)");
+                        salir = true;
+                        break;
+                    default:
+                    	clearConsole();
+                        System.out.println("\nSolo números entre 1 y 7");
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Debes insertar un número");
+                sn.nextLine();
+            }
+        }
+        sn.close();
+    }
+	
+	public void anadirArchivo() {
+		Scanner adda = new Scanner(System.in);
+        System.out.println("Introduce un nombre para el archivo\n");
+        String nombrearchivo = adda.nextLine();
+        System.out.println("Introduce una ruta para el archivo\n");
+        String arcruta = adda.nextLine();
+        System.out.println("Introduce un tamaño para el archivo\n");
+        int size = adda.nextInt();
+        Sistema.anadirArchivo(nombrearchivo, arcruta, size);
+        adda.close();
+	}
+	public void crearDir() {
+		Scanner adda = new Scanner(System.in);
+        System.out.println("Introduce un nombre para el directorio\n");
+        String nombrearchivo = adda.nextLine();
+        System.out.println("Introduce una ruta para el directorio\n");
+        String arcruta = adda.nextLine();
+        Sistema.anadirDirectorio(nombrearchivo, arcruta);
+        adda.close();
+	}
+	public void mostrarMetaDatos() {
+		Sistema.mostrarFat();
+	}
+	
+	public void mostrarDirectorio() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce la ruta del directorio\n");
+		String directo = showd.nextLine();
+		Sistema.mostrarDir(directo, 0);
+		showd.close();
+	}
+	public void eliminarArchivo() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce la ruta del archivo para eliminarlo\n");
+		String archiv = showd.nextLine();
+		Sistema.eliminarArchivo(archiv);
+		showd.close();
+	}
+	public void eliminarDir() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce la ruta del directorio para eliminarlo\n");
+		String direc = showd.nextLine();
+		Sistema.eliminarDirectorio(direc);
+		showd.close();
+	}
+
+	public void moverArchivo() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce la ruta Fuente\n");
+		String rutafuente = showd.nextLine();
+		System.out.println("Introduce la ruta Destino\n");
+		String rustdestino = showd.nextLine();
+		Sistema.moverArchivo(rutafuente, rustdestino);
+		showd.close();
+	}
+	
+	public void matarProceso() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce el nombre del proceso para eliminar\n");
+		String archiproc = showd.nextLine();
+		procesos.eliminarProceso(archiproc);
+		showd.close();
+	}
+	
+	void borrarTmp() {
+		Scanner showd = new Scanner(System.in);
+		System.out.println("Introduce un nombre.\n");
+		String name = showd.nextLine();
+		ReiniciarTMP ptmp = new ReiniciarTMP(Sistema, name);
+		ptmp.run();
+		showd.close();
+	}
+	public void clearConsole() {
+        for(int i = 0; i < 60; i++) {
+        	System.out.println("\n");
+        }
+	}
+}
